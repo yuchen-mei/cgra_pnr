f44fdfbe2d4d81bafae1524faef0d931dc9866f0
yuchen-mei <yuchen.mei@outlook.com>
2025-02-12 13:12:33 -0800

    update ci to use aha flow

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,37 +3,59 @@ set -e
 
 if [[ "$OS" == "linux" ]]; then
     if [[ "$BUILD_WHEEL" == true ]]; then
-        docker pull keyiz/manylinux-igraph
-        docker run -d --name manylinux --rm -it --mount type=bind,source="$(pwd)"/../cgra_pnr,target=/cgra_pnr keyiz/manylinux-igraph bash
+        # 1. Pull the Docker image
+        docker pull stanfordaha/garnet:latest
 
-        docker exec -i manylinux bash -c 'set -e; 
-            file_dir=/cgra_pnr; 
-            
-            # Build placer
-            build_dir=${file_dir}/thunder/build;
-            mkdir -p ${build_dir};
-            cd ${build_dir};
-            cmake .. -DCMAKE_BUILD_TYPE=Release;
-            make -j placer;
-            cd ..;
+        # 2. Run the container, mounting the local cgra_pnr repo
+        docker run -d --name garnet_container --rm -it \
+            stanfordaha/garnet:latest bash
 
-            # Build router
-            build_dir=${file_dir}/cyclone/build;
-            mkdir -p ${build_dir};
-            cd ${build_dir};
-            cmake .. -DCMAKE_BUILD_TYPE=Release;
-            make -j router;
-            cd ..;'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && python setup.py bdist_wheel'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && auditwheel show dist/*'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && auditwheel repair dist/*'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && pip install wheelhouse/*'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/cyclone && python setup.py bdist_wheel'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/cyclone && auditwheel show dist/*'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/cyclone && auditwheel repair dist/*'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/cyclone && pip install wheelhouse/*'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/ && pip install pytest && pytest -v tests/'
-        docker exec -i manylinux bash -c 'cd /cgra_pnr && mkdir wheelhouse && cp thunder/wheelhouse/* wheelhouse/ && cp cyclone/wheelhouse/* wheelhouse'
+        # 3. Checkout the current branch inside the container
+        docker exec -i garnet_container bash -c '
+            git config --global --add safe.directory /aha/cgra_pnr
+            set -e
+            cd /aha/cgra_pnr
+            CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+            git checkout "$CURRENT_BRANCH"
+        '
+
+        # 4. Build the placer (thunder)
+        docker exec -i garnet_container bash -c '
+            set -e
+            cd /aha/cgra_pnr/thunder
+            mkdir -p build
+            cd build
+            cmake .. -DCMAKE_BUILD_TYPE=Release
+            make -j placer
+        '
+
+        # 5. Build the router (cyclone)
+        docker exec -i garnet_container bash -c '
+            set -e
+            cd /aha/cgra_pnr/cyclone
+            mkdir -p build
+            cd build
+            cmake .. -DCMAKE_BUILD_TYPE=Release
+            make -j router
+        '
+
+        # 6. pip install -e for thunder and cyclone
+        docker exec -i garnet_container bash -c '
+            set -e
+            cd /aha/cgra_pnr/thunder
+            pip install -e .
+            cd /aha/cgra_pnr/cyclone
+            pip install -e .
+        '
+
+        # 7. Run aha map and aha pnr
+        docker exec -i garnet_container bash -c '
+            set -e
+            cd /aha/cgra_pnr
+            source /aha/bin/activate
+            aha map apps/pointwise
+            aha pnr apps/pointwise --width 4 --height 4
+        '
     fi
 
 elif [[ "$OS" == "osx" ]]; then
